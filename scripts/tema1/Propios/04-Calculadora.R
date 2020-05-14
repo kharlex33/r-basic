1c67edc99bec6829936ed9904dda19fd73833b77
Kharlos Pinedo <kharl20ster@gmail.com>
2020-05-13 21:07:26 -0500

    Create 04-Calculadora.R

---
+++ scripts/tema1/Propios/04-Calculadora.R
@@ -0,0 +1,61 @@
+sqrt(25)
+
+exp(1)
+
+log(pi)
+
+log(32,2)
+
+abs(-pi)
+
+factorial(7)
+
+factorial(4)
+
+factorial(0)
+
+choose(5,3)
+
+6^log(4,6)
+
+log(4^6,4)
+
+choose(3,1)
+
+sin(60*pi/180)
+
+cos(60*pi/180)
+
+sinpi(1/2)  # = sin(pi/2)
+
+tan(pi) # -1.224647
+
+tan(pi/2)
+
+asin(0.8660254) # arc sin en radianes
+
+asin(5) # arc sin x in [-1,1]
+
+print(sqrt(2),10)
+round(sqrt(2),3)
+ceiling(sqrt(2))
+trunc(sqrt(2))
+
+sqrt(2)
+
+sqrt(2)^2-2
+
+2^50
+print(2^50,15)
+
+x = (pi^2)/2
+x
+
+y <- cos(pi/4)
+y
+
+sin(pi/4) + cos(pi/4) -> z
+z
+
+edad <- 30
+nombre = "Kharlos Pinedo"
